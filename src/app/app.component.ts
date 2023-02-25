70953b5528e53c98b2eb5861d4861d4b760cd0ce
Marthin <marthinchristianz@gmail.com>
2023-02-26 00:33:10 +0800

    Update index html & css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'who-am-i-app';
+  title = 'Marthin CH';
+
+  anio: number = new Date().getFullYear();
 }
